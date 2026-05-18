eff167a8a957ab05522b902811e57b945f5cd300
harsh-blupin <harsh@bluelupin.com>
2026-05-18 14:04:55 +0530

    fix

--- web/ecosystem.config.cjs
+++ web/ecosystem.config.cjs
@@ -14,9 +14,6 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
       },
-      log_date_format: 'YYYY-MM-DD HH:mm Z',
-      error_file: './logs/error.log',
-      out_file: './logs/out.log',
       merge_logs: true,
       autorestart: true,
       watch: false,
