a4ae58d72839cdd3d66286e356c74f408cc65088
amankumarprojects <amankumarprojects@gmail.com>
2026-06-26 23:04:34 +0530

    Footer UI Fixes

--- web/src/shared/ui/layout/Footer.tsx
+++ web/src/shared/ui/layout/Footer.tsx
@@ -60,7 +60,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray300">
       <TrustBadgeSection />
-      <PageContainer className="flex flex-col gap-20 pb-16 lg:gap-[120px] lg:pb-100">
+      <PageContainer className="flex flex-col gap-20 lg:gap-[120px] lg:py-100 py-16">
         <div className="flex flex-col items-center xl:gap-12 md:gap-[40px] gap-8 lg:flex-row lg:items-start lg:justify-start">
           <div className="shrink-0">
             <Link href="/" aria-label="Sunny Diamonds">
