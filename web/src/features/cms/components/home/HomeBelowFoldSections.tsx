085029f66c3863c46eb9770449fafa6c835201c5
amankumarprojects <121786477+amankumarprojects@users.noreply.github.com>
2026-07-24 16:50:57 +0530

    Fixes

--- web/src/features/cms/components/home/HomeBelowFoldSections.tsx
+++ web/src/features/cms/components/home/HomeBelowFoldSections.tsx
@@ -70,7 +70,7 @@ export default function HomeBelowFoldSections() {
         <DiamondSourcingSection id="flawless" />
       </LazyHomeSection>
 
-      <LazyHomeSection minHeight="xl:min-h-[800px] min-h-[700px]">
+      <LazyHomeSection minHeight="">
         <FeaturedCollectionSection
           id="alankara"
           description={homeContent.alankara.collection.description}
